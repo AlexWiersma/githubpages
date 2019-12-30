c92618ea8d101e1b025c5354ee9aa83d519bdc32
Alex Wiersma <alexwiersma@gmail.com>
2019-12-30 18:00:41 +0100

    dataset_handling: Portfolio splitting

---
+++ components/portfolio/portfolio.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="arlo_tm_section relative" id="portfolio">
+    <div class="arlo_tm_portfolio_wrapper_all">
+      <!-- PORTFOLIO FILTER -->
+      <div class="arlo_tm_second_portfolio">
+        <div class="container">
+          <div class="arlo_tm_portfolio_wrap">
+            <div class="arlo_tm_title_holder portfolio" style="margin-top: 24px;">
+              <h3>Portfolio</h3>
+              <span>Check out my latest work</span>
+            </div>
+            <div class="arlo_tm_portfolio_titles"></div>
+          </div>
+        </div>
+      </div>
+      <!-- /PORTFOLIO FILTER -->
+    </div>
+  </div>
+</template>
+<script>
+export default {
+
+
+
+}
+</script>
